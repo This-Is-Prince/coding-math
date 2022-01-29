7a3589fa01c08a8e88dba3094b302d92e6e5f01c
Prince <this.is.kumar.prince@gmail.com>
2022-01-29 12:26:13 +0530

    acceleration completed

--- src/2.Coding_Math/9.Acceleration/app.ts
+++ src/2.Coding_Math/9.Acceleration/app.ts
@@ -1,4 +1,5 @@
-import { Particle } from "../Particle";
+import { MathUtils } from "../Math";
+import { CircleFill } from "../Shapes/Circles/CircleFill";
 import { Utils } from "../Utils";
 import { Vector2 } from "../Vector";
 
@@ -11,81 +12,64 @@ window.addEventListener("load", () => {
     .setCanvasBackgroundColor("white")
     .setCanvasSize(window.innerWidth - 50, window.innerHeight - 50);
 
-  // ===========First=============
-  /* 
-  // Resize Events
   window.addEventListener("resize", () => {
     ctx.setCanvasSize(window.innerWidth - 50, window.innerHeight - 50);
-    p = new Particle(100, ctx.canvas.height, 10, -Math.PI / 2);
-    accel = new Vector2(0.1, 0.1);
+    createBalls();
   });
 
-  //  Particle
-  let p = new Particle(100, ctx.canvas.height, 10, -Math.PI / 2);
-  let accel = new Vector2(0.1, 0.1);
-
-  // Update
-  function update() {
-    ctx.clearCanvas();
-
-    p.accelerate(accel);
-    p.update();
-
-    ctx.beginPath();
-    ctx.arc(p.position.x, p.position.y, 10, 0, Math.PI * 2, false);
-    ctx.fill();
-
-    window.requestAnimationFrame(update);
-  }
-  update(); */
-
-  // ===========Second=============
-  // Resize
-  window.addEventListener("resize", () => {
-    ctx.setCanvasSize(window.innerWidth - 50, window.innerHeight - 50);
-    createParticles();
-  });
-
-  // Particles
-  let particles: Particle[] = [];
-  const totalParticles = 100;
-  const gravity = new Vector2(0, 0.1);
-
-  function createParticles() {
-    const tempArr: Particle[] = [];
-    for (let i = 0; i < totalParticles; i++) {
-      tempArr.push(
-        new Particle(
-          ctx.canvas.width / 2,
-          ctx.canvas.height / 3,
-          Math.random() * 5 + 2,
-          Math.random() * Math.PI * 2,
-          gravity
-        )
+  let balls: Ball[] = [];
+  function createBalls() {
+    const totalBalls = 100;
+    const tempBalls: Ball[] = [];
+    for (let i = 0; i < totalBalls; i++) {
+      const ball = new Ball(
+        ctx.canvas.width / 2,
+        ctx.canvas.height / 3,
+        MathUtils.randomInt(2, 5)
       );
+      ball.applyVelocity(Math.random() * 5 + 2, Math.random() * Math.PI * 2);
+      ball.applyAcceleration(0.1, Math.PI * 0.5);
+      tempBalls.push(ball);
     }
-    particles = tempArr;
+    balls = tempBalls;
   }
-  createParticles();
+  createBalls();
 
   // Update
   function update() {
     ctx.clearCanvas();
-    particles.forEach((particle) => {
-      //   particle.accelerate(gravity);
-      particle.update();
-      ctx.beginPath();
-      ctx.arc(
-        particle.position.x,
-        particle.position.y,
-        5,
-        0,
-        Math.PI * 2,
-        false
-      );
-      ctx.fill();
+    balls.forEach((ball) => {
+      ball.draw(ctx);
     });
     window.requestAnimationFrame(update);
   }
   update();
 });
+
+class Ball {
+  private shape!: CircleFill;
+  private velocity!: Vector2;
+  private acceleration!: Vector2;
+  constructor(x: number, y: number, radius: number) {
+    this.shape = new CircleFill(x, y, radius);
+    this.velocity = new Vector2(0, 0);
+    this.acceleration = new Vector2(0, 0);
+  }
+  public applyVelocity(speed: number, angle: number) {
+    this.velocity.setLength(speed);
+    this.velocity.setAngle(angle);
+  }
+  public applyAcceleration(mag: number, angle: number) {
+    this.acceleration.setLength(mag);
+    this.acceleration.setAngle(angle);
+  }
+  private updateVelocity() {
+    this.velocity.addTo(this.acceleration);
+    this.shape.position.x += this.velocity.x;
+    this.shape.position.y += this.velocity.y;
+  }
+  public draw(ctx: CanvasRenderingContext2D) {
+    this.updateVelocity();
+    this.shape.draw(ctx);
+  }
+}
