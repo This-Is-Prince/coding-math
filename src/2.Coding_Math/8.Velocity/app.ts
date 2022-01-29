ccd385902117af3bcd969ecfed30a179deadfb26
Prince <this.is.kumar.prince@gmail.com>
2022-01-29 11:52:12 +0530

    velocity lecture updated

--- src/2.Coding_Math/8.Velocity/app.ts
+++ src/2.Coding_Math/8.Velocity/app.ts
@@ -1,5 +1,6 @@
-import { Particle } from "../Particle";
-import { Canvas2DContext, Utils } from "../Utils";
+import { MathUtils } from "../Math";
+import { CircleFill } from "../Shapes/Circles/CircleFill";
+import { Utils } from "../Utils";
 import { Vector2 } from "../Vector";
 
 window.addEventListener("load", () => {
@@ -13,113 +14,58 @@ window.addEventListener("load", () => {
 
   window.addEventListener("resize", () => {
     ctx.setCanvasSize(window.innerWidth - 50, window.innerHeight - 50);
-    createParticle();
+    createBalls();
   });
 
-  //   ==========first==========
-  /*   // Ball
-const ball = new Ball(100, 100, 10);
-const velocity = new Velocity(0, 0);
-velocity.setSpeed(0.2);
-//   velocity.setDirection(Math.PI / 6);
-
-update();
-function update() {
-    ctx.clearCanvas();
-    ball.applyVelocity(velocity);
-    ball.draw(ctx);
-    window.requestAnimationFrame(update);
-} */
-
-  //   ==========second==========
-  /*   const p = new Particle(100, 100, 3, Math.PI / 6);
-update();
-function update() {
-    ctx.clearCanvas();
-    p.update();
-    ctx.beginPath();
-    ctx.arc(p.position.x, p.position.y, 10, 0, Math.PI * 2, false);
-    ctx.fill();
-    window.requestAnimationFrame(update);
-} */
-
-  //   ==========third==========
-  let particles: Particle[] = [];
-  const totalParticles = 100;
-
-  function createParticle() {
-    const tempArr: Particle[] = [];
-    for (let i = 0; i < totalParticles; i++) {
-      tempArr.push(
-        new Particle(
-          ctx.canvas.width / 2,
-          ctx.canvas.height / 2,
-          Math.random() * 4 + 1,
-          Math.random() * Math.PI * 2
-        )
+  let balls: Ball[] = [];
+  function createBalls() {
+    const totalBalls = 100;
+    const tempBalls: Ball[] = [];
+    for (let i = 0; i < totalBalls; i++) {
+      const ball = new Ball(
+        ctx.canvas.width / 2,
+        ctx.canvas.height / 2,
+        MathUtils.randomInt(2, 7)
       );
+      ball.applyVelocity(
+        MathUtils.randomInt(1, 5),
+        MathUtils.randomRange(0, Math.PI * 2)
+      );
+      tempBalls.push(ball);
     }
-    particles = tempArr;
+    balls = tempBalls;
   }
-  createParticle();
+  createBalls();
+
   function update() {
     ctx.clearCanvas();
-    particles.forEach((particle) => {
-      particle.update();
-      ctx.beginPath();
-      ctx.arc(
-        particle.position.x,
-        particle.position.y,
-        10,
-        0,
-        Math.PI * 2,
-        false
-      );
-      ctx.fill();
+    balls.forEach((ball) => {
+      ball.draw(ctx);
     });
+
     window.requestAnimationFrame(update);
   }
   update();
 });
 
-class Velocity {
-  public vec!: Vector2;
-  constructor(x: number, y: number) {
-    this.vec = new Vector2(x, y);
-  }
-  public setSpeed(value: number) {
-    this.vec.setLength(value);
-  }
-  public setDirection(angle: number) {
-    this.vec.setAngle(angle);
-  }
-}
-
 class Ball {
-  public position!: Vector2;
-  public radius!: number;
+  private shape!: CircleFill;
+  private velocity!: Vector2;
   constructor(x: number, y: number, radius: number) {
-    this.position = new Vector2(x, y);
-    if (radius <= 0) {
-      throw new Error(`radius of ball can't be negative or zero`);
-    }
-    this.radius = radius;
+    this.shape = new CircleFill(x, y, radius);
+    this.velocity = new Vector2(0, 0);
   }
-
-  public applyVelocity(velocity: Velocity) {
-    this.position.addTo(velocity.vec);
+  public applyVelocity(speed: number, angle: number) {
+    this.velocity = new Vector2();
+    this.velocity.setLength(speed);
+    this.velocity.setAngle(angle);
   }
-
-  public draw(ctx: Canvas2DContext) {
-    ctx.beginPath();
-    ctx.arc(
-      this.position.x,
-      this.position.y,
-      this.radius,
-      0,
-      Math.PI * 2,
-      false
-    );
-    ctx.fill();
+  private updatePosition() {
+    this.shape.position.x += this.velocity.x;
+    this.shape.position.y += this.velocity.y;
+  }
+  public draw(ctx: CanvasRenderingContext2D) {
+    this.updatePosition();
+    this.shape.draw(ctx);
   }
 }
